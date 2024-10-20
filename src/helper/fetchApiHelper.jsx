35400b72e35e6441294a956b3d05cafcb4609028
chndnnn <chndn136@gmail.com>
2024-10-20 18:03:16 +0530

    added local env variables

--- src/helper/fetchApiHelper.jsx
+++ src/helper/fetchApiHelper.jsx
@@ -2,15 +2,16 @@ import axios from "axios";
 // import data from "./test.json";
 
 export async function fetchNews(category) {
+  const Api_key = import.meta.env.VITE_API_KEY;
   try {
     if (!category) {
       let data = await axios.get(
-        `https://gnews.io/api/v4/top-headlines?lang=hi&country=in&max=10&apikey=ca77a13df23d97b9c6a7219f3671acd7`
+        `https://gnews.io/api/v4/top-headlines?lang=hi&country=in&max=10&apikey=${Api_key}`
       );
       return data.data.articles;
     } else {
       let data = await axios.get(
-        `https://gnews.io/api/v4/top-headlines?category=${category}&lang=hi&country=in&max=10&apikey=ca77a13df23d97b9c6a7219f3671acd7`
+        `https://gnews.io/api/v4/top-headlines?category=${category}&lang=hi&country=in&max=10&apikey=${Api_key}`
       );
       return data.data.articles;
     }
