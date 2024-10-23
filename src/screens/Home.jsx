1e6643b4c41d62ab8b24ba257f0a577008467ef4
chndnnn <chndn136@gmail.com>
2024-10-23 16:24:42 +0530

    removed unnecessary comments

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -3,12 +3,6 @@ import { useQuery } from "@tanstack/react-query";
 import Card from "../components/Card";
 
 const Home = () => {
-  // let [newsData, setNewsData] = useState();
-
-  // useEffect(() => {
-  //   fetchNews();
-  // }, []);
-
   const { data, isPending, isError } = useQuery({
     queryKey: ["Home"],
     queryFn: () => fetchNews(),
@@ -24,18 +18,6 @@ const Home = () => {
     return <h2>Something went wrong</h2>;
   }
 
-  // async function fetchNews() {
-  //   try {
-  //     let data = await axios.get(
-  //       `https://gnews.io/api/v4/top-headlines?lang=hi&country=in&max=10&apikey=ca77a13df23d97b9c6a7219f3671acd7`
-  //     );
-  //     setNewsData(data.data.articles);
-
-  //     console.log(data);
-  //   } catch (err) {
-  //     console.log(err);
-  //   }
-  // }
   return (
     <div>
       {data?.map((ele) => {
